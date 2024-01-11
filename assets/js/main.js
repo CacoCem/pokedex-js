f2b3950f08d10f98e5c1be856fa09349c6f305d4
Cauan <cacocembritosoares@gmail.com>
2024-01-11 16:15:13 -0300

    Atualização

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,27 @@
-console.log('BOLO!!!!');
+// Fazer a requisição da API PokeAPI
+const offset = 0;
+const limit = 10;
+const url = `https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`;
+
+function convertPokemonToList(pokemon) {
+    return `
+    <li class="pokemon">
+                <span class="number">#001</span>
+                <span class="name">${pokemon.name}</span>
+                <div class="detail">
+                    <ol class="types">
+                        <li class="type">grass</li>
+                        <li class="type">poison</li>
+                    </ol>
+                    <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png" alt="${pokemon.name}">
+                </div>
+            </li> 
+    `
+}
+
+const pokemonList = document.getElementById('pokemonList')
+
+// Iniciar a requisição usando fetch (promise)
+pokeApi.getPokemons().then((pokemons = []) => {
+    pokemonList.innerHTML += pokemons.map(convertPokemonToList).join('')
+})
