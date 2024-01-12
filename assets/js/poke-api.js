5251d95698a3801d8ba94d0f9c4cc30b9c84e953
Cauan <cacocembritosoares@gmail.com>
2024-01-12 15:47:19 -0300

    Atualização dos arquivos JS

--- assets/js/poke-api.js
+++ assets/js/poke-api.js
@@ -1,22 +1,34 @@
 const pokeApi = {}
 
-pokeApi.getPokemons = (offset = 0, limit = 10) => {
-    // Fazer a requisição da API PokeAPI
-    const url = `https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`
-    return fetch(url)
-        .then((response) => response.json())
-        .then((jsonBody) => jsonBody.results)
-        .then((pokemons) => pokemons.map((pokemon) => fetch(pokemon.url)))
+function convertPokeApiDetailToPokemon(pokeDetail) {
+    const pokemon = new Pokemon()
+    pokemon.number = pokeDetail.order
+    pokemon.name = pokeDetail.name
+    const types = pokeDetail.types.map((typeSlot) => typeSlot.type.name)
+    const [type] = types
+    pokemon.types = types
+    pokemon.type = type
+    pokemon.photo = pokeDetail.sprites.other['official-artwork'].front_default
+    return pokemon
 }
 
 
+// Função para obter detalhes de um Pokémon específico
+pokeApi.getPokemonDetail = (pokemon) => {
+    return fetch(pokemon.url)
+        .then((response) => response.json())
+        .then(convertPokeApiDetailToPokemon)
+}
 
+// Função para obter uma lista de Pokémon
+pokeApi.getPokemons = (offset = 0, limit = 5) => {
+    // Fazer a requisição da API PokeAPI
+    const url = `https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`
 
-    Promise.all([
-        fetch('https://pokeapi.co/api/v2/pokemon/1'),
-        fetch('https://pokeapi.co/api/v2/pokemon/2'),
-        fetch('https://pokeapi.co/api/v2/pokemon/3'),
-        fetch('https://pokeapi.co/api/v2/pokemon/4')
-    ]).then((results) => {
-        console.log(results);
-    })
+    return fetch(url)
+        .then((response) => response.json())                            // Converte a resposta para JSON
+        .then((jsonBody) => jsonBody.results)                           // Extrai a lista de Pokémon da resposta JSON
+        .then((pokemons) => pokemons.map(pokeApi.getPokemonDetail))     // Mapeia cada Pokémon para obter detalhes
+        .then((detailRequests) => Promise.all(detailRequests))          // Aguarda todas as solicitações de detalhes serem concluídas
+        .then((pokemonsDetails) => pokemonsDetails)                     // Retorna os detalhes de cada Pokémon
+}
