f804f6ddeb0b8f1a5ed981517637bb242f5e31e6
Cauan <cacocembritosoares@gmail.com>
2024-01-16 14:58:02 -0300

    Adicionando funcionalidades

--- assets/js/details.js
+++ assets/js/details.js
@@ -13,41 +13,95 @@ document.addEventListener('DOMContentLoaded', () => {
     }
 });
 
+const getColorClass = (statValue) => {
+    if (statValue < 50) {
+        return 'red';
+    } else if (statValue === 50) {
+        return 'yellow';
+    } else {
+        return 'green';
+    }
+};
+
 function displayPokemonDetails(pokemon) {
     // Seletor para o elemento onde os detalhes do Pokémon serão exibidos
     const pokemonDetailsContainer = document.getElementById('pokemonDetails');
 
     // Cria elementos HTML para exibir os detalhes do Pokémon
     const pokemonDetailsHtml = `
-        <div class="pokemon-detail ${pokemon.type}">
-            <div class="header-detail">
-                <h2>${pokemon.name}</h2>
-                <span class="number">#${pokemon.number.toString().padStart(3, '0')}</span>
-            </div>
-            <div class="detail">
-                <ol class="types">
-                    ${pokemon.types.map((type) => `<li class="type ${type}">${type}</li>`).join('')}
-                </ol>
-                <div class="img-detail">
-                    <img src=${pokemon.photo} alt="${pokemon.name}">
-                </div>
-            </div>
-            <div class="stats-pokemon">
-                <h2 class="manteiga">Sobre</h2>
-                <ol>
-                    <li>Altura: ${pokemon.height/10} cm</li>
-                    <li>Peso: ${pokemon.weight/10} kg</li>
-                    <li>Stats:</li>
-                    <li>HP: ${pokemon.stats.hp}</li>
-                    <li>Ataque: ${pokemon.stats.attack}</li>
-                    <li>Defesa: ${pokemon.stats.defense}</li>
-                    <li>Ataque especial: ${pokemon.stats['special-attack']}</li>
-                    <li>Defesa especial: ${pokemon.stats['special-defense']}</li>
-                    <li>Velocidade: ${pokemon.stats.speed}</li>
-                </ol>
+    <div class="pokemon-detail ${pokemon.type}">
+        <div class="header-detail">
+            <h2 class="name"> ${pokemon.name}</h2>
+            <span class="number">#${pokemon.number.toString().padStart(3, '0')}</span>
+        </div>
+        <div class="detail">
+            <ol class="types">
+                ${pokemon.types.map((type) => `<li class="type ${type}">${type}</li>`).join('')}
+            </ol>
+            <div class="img-detail">
+                <img src=${pokemon.photo} alt="${pokemon.name}">
             </div>
         </div>
-    `;
+        <div class="stats-pokemon">
+            <h2 class="sobre-pokemon">Sobre</h2>
+            <ol>
+                <li>Altura: ${pokemon.height/10} m</li>
+                <li>Peso: ${pokemon.weight/10} kg</li>
+                <br>
+                <li>Stats:</li>
+                <li>HP
+                    <div class="stats-progresso">
+                        <span>${pokemon.stats.hp}</span>
+                        <div class="barra">
+                            <div class="barra-progresso ${getColorClass(pokemon.stats.hp)}" style="width: ${pokemon.stats.hp}%; max-width: 100%"></div>
+                        </div>  
+                    </div>               
+                </li>
+                <li>Ataque
+                    <div class="stats-progresso">
+                        <span>${pokemon.stats.attack}</span>
+                        <div class="barra">
+                            <div class="barra-progresso ${getColorClass(pokemon.stats.attack)}" style="width: ${pokemon.stats.attack}%; max-width: 100%"></div>
+                        </div> 
+                    </div>
+                </li>
+                <li>Defesa
+                    <div class="stats-progresso">
+                        <span>${pokemon.stats.defense}</span>
+                        <div class="barra">
+                            <div class="barra-progresso ${getColorClass(pokemon.stats.defense)}" style="width: ${pokemon.stats.defense}%; max-width: 100%"></div>
+                        </div> 
+                        
+                    </div>
+                </li>
+                <li>Ataque Especial
+                    <div class="stats-progresso">
+                        <span>${pokemon.stats['special-attack']}</span>
+                        <div class="barra">
+                            <div class="barra-progresso ${getColorClass(pokemon.stats['special-attack'])}" style="width: ${pokemon.stats['special-attack']}%; max-width: 100%"></div>
+                        </div>
+                    </div>
+                </li>
+                <li>Defesa Especial
+                    <div class="stats-progresso">
+                        <span>${pokemon.stats['special-defense']}</span>
+                        <div class="barra">
+                            <div class="barra-progresso ${getColorClass(pokemon.stats['special-defense'])}" style="width: ${pokemon.stats['special-defense']}%; max-width: 100%"></div>
+                        </div>
+                    </div>                    
+                </li>
+                <li>Velocidade
+                    <div class="stats-progresso">
+                        <span>${pokemon.stats.speed}</span>
+                        <div class="barra">
+                            <div class="barra-progresso ${getColorClass(pokemon.stats.speed)}" style="width: ${pokemon.stats.speed}%; max-width: 100%"></div>
+                        </div>
+                    </div>
+                </li>
+            </ol>
+        </div>
+    </div>
+`;
 
     // Adiciona os detalhes do Pokémon ao container criado
     pokemonDetailsContainer.innerHTML = pokemonDetailsHtml;
